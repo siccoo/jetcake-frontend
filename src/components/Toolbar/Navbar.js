c145a0fd30b4d8cd579f2497fbba03f1e4da15a6
siccoo <obichilaka@gmail.com>
2020-01-27 13:40:48 +0100

    Structuring the navbar section

--- src/components/Toolbar/Navbar.js
+++ src/components/Toolbar/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Navbar.css';
 
 const navbar = props => (
-    <header>
-        <nav>
-            
+    <header className="navbar">
+        <nav className="navbar_nav">
+            <div></div>
+            <div className="navbar_logo">
+                <a href="/">Jet Cake Frontend </a>
+            </div>
+            <div className="navbar_navitem">
+                <ul>
+                    <li><a href="/">Sign Up</a></li>
+                    <li><a href="/">Login</a></li>
+                </ul> 
+            </div>
         </nav>
     </header>
 );
+
+export default navbar;
