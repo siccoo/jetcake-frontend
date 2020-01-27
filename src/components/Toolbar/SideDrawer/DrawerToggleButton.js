59fa75b3213044c75282c1c7dbb0f4f563674661
siccoo <obichilaka@gmail.com>
2020-01-27 15:50:55 +0100

    Update on DrawerToggleButton.js

---
+++ src/components/Toolbar/SideDrawer/DrawerToggleButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const drawerToggleButton = props => (
+    <button className="toggle-button">
+        <div className="button-toggle_line" />
+        <div className="button-toggle_line" />
+        <div className="button-toggle_line" />
+    </button>
+);
+
+export default drawerToggleButton;
