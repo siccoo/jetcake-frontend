16bf238820d5a1fd53a9a90f7e76b4435bbcfffb
siccoo <obichilaka@gmail.com>
2020-01-28 16:30:10 +0100

    Updates on SideDrawer.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div style={{height: '100%'}}>
       <Navbar />
       <SideDrawer />
-    </div>
+    </div> 
   );
 };
 
--- src/components/Toolbar/SideDrawer/SideDrawer.js
+++ src/components/Toolbar/SideDrawer/SideDrawer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-    <nav className="side-drawer">
+    return (
+        <nav className="side-drawer">
         <ul>
             <li><a href="/">Sign up</a></li>
             <li><a href="/">Login</a></li>
         </ul>
     </nav>
+    );  
 };
 
 export default SideDrawer;
