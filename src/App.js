976b362967ef43fdf93ea53271beadff6d800bf1
siccoo <obichilaka@gmail.com>
2020-01-28 17:20:49 +0100

    Updates on App.js, Backdrop.css & SideDrawer.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Toolbar/Navbar';
 import SideDrawer  from './components/Toolbar/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
 function App() {
   return (
     <div style={{height: '100%'}}>
       <Navbar />
       <SideDrawer />
+      <Backdrop />
     </div> 
   );
 };
