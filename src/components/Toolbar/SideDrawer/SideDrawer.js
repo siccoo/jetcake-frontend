69497f64bb52007b65b5144b4223bf6ca495c377
siccoo <obichilaka@gmail.com>
2020-01-28 15:54:17 +0100

    Update on the SideDrawer.js

---
+++ src/components/Toolbar/SideDrawer/SideDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SideDrawer = props => {
+    <nav>
+        <ul>
+            <li><a href="/">Sign up</a></li>
+            <li><a href="/">Login</a></li>
+        </ul>
+    </nav>
+}
+
+export default SideDrawer;
